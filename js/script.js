346174f89a4c6ffd1cdab2a0147f8ff1dcbb1b65
MarcoDeRosso <mcoder@hotmail.it>
2021-06-14 17:28:06 +0200

    add includes

--- js/script.js
+++ js/script.js
@@ -11,10 +11,13 @@ controllando che non siano uguali.
 richiamerò questo array con l'allert facendo vedere questi numeri */
 
 document.addEventListener('DOMContentLoaded', function() {
+    var computerNums = [];
+    var arrayUser = [];
+    var rightNumbers = [];
+
     function getRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
-    var computerNums = [];
 
     while (computerNums.length < 5) {
         var randomNum = getRandomNumber(1, 100);
@@ -22,29 +25,27 @@ document.addEventListener('DOMContentLoaded', function() {
             computerNums.push(randomNum);
         }
     }
+    alert("Attenzione! Memorizza questi numeri: " + computerNums);
     console.log(computerNums);
     /* creo una funzione con un timer di 30000 secondi al suo interno metterò un ciclo che chiederà 5 volte un numero all'utente, 
     mettendo SE il numero detto è corretto allora scrivi numero corretto se non è presente nell'arrey scrivi non corretto. */
     var result = document.querySelector(".result");
-    var arrayUser = [];
     setTimeout(function() {
         console.log('SetTimeout inizio esecuzione');
         while (arrayUser.length < 5) {
             var userNum = parseInt(prompt("Prova la tua memoria: inserisci un numero da 1 a 100!"));
             while (isNaN(userNum) || userNum < 1 || userNum > 100) {
                 alert("Attenzione solo un numero da 1 a 100")
-                var userNum = parseInt(prompt("Prova la tua memoria: inserisci un numero da 1 a 100!"));
+                var userNum = parseInt(prompt("Prova la tua memoria: inserisci un numero da 1 a 44100!"));
             }
             arrayUser.push(userNum);
-            if (arrayUser.length === computerNums.length) {
-                result.innerText += "Numero esatto!" + userNum;
-            } else {
-                result.innerText += "Numero sbagliato!" + userNum;
+            if (computerNums.includes(userNum)) {
+                rightNumbers.push(userNum);
             }
         }
-    }, 30000);
+        console.log("hai indovintato " + rightNumbers.length + " numeri, eccoli quali " + rightNumbers);
+    }, 2000);
 
 
-    alert("Attenzione! Memorizza questi numeri: " + computerNums);
 
 });
