91699dc67e6ae679123f5f35a74ca470ed614b5e
MarcoDeRosso <mcoder@hotmail.it>
2021-06-14 16:11:24 +0200

    add while

--- js/script.js
+++ js/script.js
@@ -22,8 +22,28 @@ document.addEventListener('DOMContentLoaded', function() {
             computerNums.push(randomNum);
         }
     }
-
     console.log(computerNums);
+    /* creo una funzione con un timer di 30000 secondi al suo interno metterò un ciclo che chiederà 5 volte un numero all'utente, 
+    mettendo SE il numero detto è corretto allora scrivi numero corretto se non è presente nell'arrey scrivi non corretto. */
+    var result = document.querySelector(".result");
+    var arrayUser = [];
+    setTimeout(function() {
+        console.log('SetTimeout inizio esecuzione');
+        while (arrayUser.length < 5) {
+            var userNum = parseInt(prompt("Prova la tua memoria: inserisci un numero da 1 a 100!"));
+            while (isNaN(userNum) || userNum < 1 || userNum > 100) {
+                alert("Attenzione solo un numero da 1 a 100")
+                var userNum = parseInt(prompt("Prova la tua memoria: inserisci un numero da 1 a 100!"));
+            }
+            arrayUser.push(userNum);
+            if (arrayUser.length === computerNums.length) {
+                result.innerText += "Numero esatto!" + userNum;
+            } else {
+                result.innerText += "Numero sbagliato!" + userNum;
+            }
+        }
+    }, 30000);
+
 
     alert("Attenzione! Memorizza questi numeri: " + computerNums);
 
